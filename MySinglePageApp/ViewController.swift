3a9797b41c4a7232fed3d9ec36248f312c2fbe65
b5710546232 <nattapat.su@ku.th>
2016-11-08 19:21:37 +0700

    initail Project

--- MySinglePageApp/ViewController.swift
+++ MySinglePageApp/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var someLabel: UILabel!
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +25,11 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func someAction(_ sender: Any) {
+        count+=1
+        someLabel.text = String(count)
+      
+    }
+    
 }
 
